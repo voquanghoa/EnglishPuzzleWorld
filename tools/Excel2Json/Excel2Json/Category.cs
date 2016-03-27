850f18a2dcdd058288e236c0d51fc54d8b637325
Võ Quang Hòa <voquanghoa@gmail.com>
2016-03-27 11:01:25 +0700

    Git add tool

---
+++ tools/Excel2Json/Excel2Json/Category.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel2Json
+{
+	[DataContract]
+	public class Question
+	{
+		[DataMember(Name = "title")]
+		public string Title
+		{
+			get; set;
+		}
+
+		[DataMember(Name = "answer")]
+		public string Answer
+		{
+			get; set;
+		}
+	}
+	[DataContract]
+	public class Category
+	{
+		[DataMember(Name = "name")]
+		public string Name
+		{
+			get; set;
+		}
+
+		[DataMember(Name = "questions")]
+		public List<Question> Questions
+		{
+			get; set;
+		}
+
+		public Category()
+		{
+			Questions = new List<Question>();
+		}
+	}
+	[DataContract]
+	public class Data : List<Category>
+	{
+
+	}
+}
---
+++ tools/Excel2Json/Excel2Json/Program.cs
@@ -0,0 +1,56 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Excel2Json
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Usage: <program> <excel file>");
+				return;
+			}
+
+			var excelFileName = args[0];
+			var directory = Path.GetDirectoryName(excelFileName);
+			var jsonFileName = Path.Combine(directory, Path.GetFileNameWithoutExtension (excelFileName) + ".json");
+
+			Data data = new Data();
+			foreach (var sheet in Workbook.Worksheets(args[0]))
+			{
+				foreach (var row in sheet.Rows)
+				{
+					var question = row.Cells[1].Text;
+					var answer = row.Cells[2].Text;
+					var categoryName = row.Cells[3].Text;
+
+					var category = data.FirstOrDefault(x => x.Name == categoryName);
+
+					if (category == null)
+					{
+						category = new Category();
+						category.Name = categoryName;
+						data.Add(category);
+					}
+
+					category.Questions.Add(new Question()
+					{
+						Answer = answer,
+						Title= question
+					});
+				}	
+			}
+
+			File.WriteAllText(jsonFileName, JsonConvert.SerializeObject(data));
+			
+		}
+	}
+}
